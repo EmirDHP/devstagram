70cc9672419814646e24a3fe4e02b13440827f23
Tr4nc3L1f3 <155707978+Tr4nc3L1f3@users.noreply.github.com>
2024-03-11 23:20:29 -0500

    Validations

---
+++ app/Http/Controllers/RegistrerController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegistrerController extends Controller
+{
+    //
+    public function index () 
+    {
+        return view('auth.register');
+    }
+    
+    public function store(Request $request)
+    {
+        // dd($request);    De esta manera accedo a todos lo valores
+        // dd($request->get('email'));  De esta manera solo accede al valor que quiero ver
+
+        //Validacion
+        $this->validate($request, [
+            // 'name' => 'required|max:30',
+            'name' => ['required','max:30'],
+            'username' => ['required','unique:users','min:3','max:20'],
+            'email' => ['required','unique:users','email','max:60'],
+            'password' => ['required']
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RegistrerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('principal');
 });
+
+
+Route::get('/register', [RegistrerController::class,'index'])->name('register');
+Route::post('/register', [RegistrerController::class,'store']);
