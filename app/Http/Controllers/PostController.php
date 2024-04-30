5ffce6a26d9152c30de393dbd49c28c8db0be2de
EmirDHP <emir.perez@outlook.es>
2024-04-29 23:31:07 -0500

    Dynamics URls

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -12,9 +13,12 @@ class PostController extends Controller
         $this->middleware('auth');    
     }
 
-    public function index()
+    public function index(User $user)
     {
         // dd(auth()->user());
-        return view('dashboard');
+        // dd($user->username);
+        return view('dashboard',[
+            'user'=> $user
+        ]);
     }
 }
