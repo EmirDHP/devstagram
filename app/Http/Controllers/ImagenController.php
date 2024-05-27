1d2ec126fad05b7aa4e101bf86b6f22fd975a41e
EmirDHP <emir.perez@outlook.es>
2024-05-26 21:41:15 -0500

    Request Reading

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -7,8 +7,13 @@ use Illuminate\Http\Request;
 class ImagenController extends Controller
 {
     //
-    public function store()
+    public function store(Request $request)
     {
-        return "Desde imagen controller";
+        // return "Desde imagen controller";
+        // $input = $request->all();
+        $imagen = $request->file('file');
+
+
+        return response()->json(['imagen' => $imagen->extension()]);
     }
 }
