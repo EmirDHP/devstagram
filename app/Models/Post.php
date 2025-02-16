cc23843059b0369955e15b70dd883046bd8796b7
EmirDHP <emir.perez@outlook.es>
2025-02-16 00:29:23 -0600

    Save and show comments

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
@@ -20,4 +21,9 @@ class Post extends Model
     {
         return $this->belongsTo(User::class)->select(['name', 'username']);
     }
+
+    public function comentarios(){
+        return $this->hasMany(Comentario::class);
+    }
+
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -25,6 +25,12 @@
                 @auth
                 <p class="text-xl font-bold text-center mb-4">Agrega un Nuevo Comentario</p>
 
+                @if (session('mensaje'))
+                    <div class="bg-green-500 p-2 rounded-lg mb-6 text-white text-center uppercase font-bold">
+                        {{ session('mensaje') }}
+                    </div>                    
+                @endif
+
                 <form action="{{ route('comentarios.store', ['post' => $post, 'user' => $user ]) }}" method="POST">
                     @csrf
                     <div class="mb-5">
@@ -51,6 +57,23 @@
                     />
                 </form>
                 @endauth
+
+                <div class="bg-white shadow mb-5 max-h-96 overflow-y-scroll mt-10">
+                    @if ($post->comentarios->count())
+                        @foreach ($post->comentarios as $comentario)
+                            <div class="p-5 border-gray-300 border-b">
+                                <a href="{{ route('posts.index', $comentario->user) }}" class="font-bold">
+                                    {{ $comentario->user->username }}
+                                </a>
+                                <p>{{ $comentario->comentario }}</p>
+                                <p class="text-sm text-gray-500">{{ $comentario->created_at->diffForHumans() }}</p>
+                            </div>                            
+                        @endforeach
+                    @else
+                        <p class="p-10 text-center"> No hay comentarios aún</p>
+                    @endif
+                </div>
+
             </div>
         </div>
     </div>
