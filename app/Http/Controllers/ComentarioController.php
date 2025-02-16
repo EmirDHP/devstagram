52576c9c5468c32c0b48a6a8523c84971d3e89c4
EmirDHP <emir.perez@outlook.es>
2025-02-15 23:45:38 -0600

    Adding Comments

---
+++ app/Http/Controllers/ComentarioController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ComentarioController extends Controller
+{
+    public function store(){
+        // Validar
+
+        // Almacenar el resultado
+
+        // Imprimir un mensaje
+    }
+}
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('contenido')
-    <div class="container mx-auto flex">
+    <div class="container mx-auto md:flex">
         <div class="md:w-1/2">
             <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
             
@@ -19,8 +19,39 @@
                 <p class="mt-5">{{ $post->descripcion }}</p>
             </div>
         </div>
-        <div class="md:w-1/2">
-            
+        <div class="md:w-1/2 p-5">
+            <div class="shadow bg-white p-5 mb-5">
+
+                @auth
+                <p class="text-xl font-bold text-center mb-4">Agrega un Nuevo Comentario</p>
+
+                <form action="{{ route('comentarios.store', ['post' => $post, 'user' => $user ]) }}" method="POST">
+                    @csrf
+                    <div class="mb-5">
+                        <label for="titulo" class="mb-2 block uppercase text-gray-500 font-bold">
+                            Comentario
+                        </label>
+                        <textarea 
+                            id="comentario" 
+                            name="comentario"
+                            placeholder="Agregar un comentario" 
+                            class="border p-2 w-full rounded-lg @error('name') border-red-500 @enderror"
+                            
+                        >{{ old('comentario') }}</textarea>
+    
+                        @error('comentario')
+                            <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <input 
+                        type="submit"
+                        value="Comentar"
+                        class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
+                    />
+                </form>
+                @endauth
+            </div>
         </div>
     </div>
 @endsection
